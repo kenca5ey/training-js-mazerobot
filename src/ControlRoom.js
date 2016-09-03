27f47ce3936dfa3d3689211fb885a57cc1cdfb4c
Casey <kcasey@deloitte.ie>
2016-09-03 03:39:26 +0100

    Quick changes to get robot outside of maze

--- src/ControlRoom.js
+++ src/ControlRoom.js
@@ -39,3 +39,44 @@ export function pathAhead(position, direction) {
         log: 'I am moving!'
     };
 }
+
+export function moveInDirection(position, direction) {
+
+    if (direction === 'east') {
+        return {
+            position: {
+                x: position.x + 1,
+                y: position.y
+            },
+            direction: 'east',
+            log: 'I am moving east!'
+        };
+    } else if (direction === 'south') {
+        return {
+            position: {
+                x: position.x,
+                y: position.y + 1
+            },
+            direction: 'south',
+            log: 'I am moving south!'
+        };
+    } else if (direction === 'west') {
+        return {
+            position: {
+                x: position.x - 1,
+                y: position.y
+            },
+            direction: 'west',
+            log: 'I am moving west!'
+        };
+    } else if (direction === 'north') {
+        return {
+            position: {
+                x: position.x,
+                y: position.y - 1
+            },
+            direction: 'north',
+            log: 'I am moving north!'
+        };
+    }
+}
--- src/Controller.js
+++ src/Controller.js
@@ -1,5 +1,5 @@
 import {matrix, types} from './Config';
-import {obstacleAhead, pathAhead} from './ControlRoom';
+import {obstacleAhead, pathAhead, moveInDirection} from './ControlRoom';
 
 export function isPositionAvailable(matrix, position) {
     return typeof matrix[position.y] !== 'undefined'
@@ -32,8 +32,92 @@ export function isPositionInFrontAvailable(matrix, position, direction) {
     }
 }
 
+export function isPositionToLeftOfTravelAvailable(matrix, position, direction) {
+    switch (direction) {
+        case 'east':
+            return isPositionAvailable(matrix, {
+                x: position.x,
+                y: position.y - 1
+            });
+        case 'south':
+            return isPositionAvailable(matrix, {
+                x: position.x + 1,
+                y: position.y
+            });
+        case 'west':
+            return isPositionAvailable(matrix, {
+                x: position.x,
+                y: position.y + 1
+            });
+        case 'north':
+            return isPositionAvailable(matrix, {
+                x: position.x - 1,
+                y: position.y
+            });
+    }
+}
+
+export function isPositionToRightOfTravelAvailable(matrix, position, direction) {
+    switch (direction) {
+        case 'east':
+            return isPositionAvailable(matrix, {
+                x: position.x,
+                y: position.y + 1
+            });
+        case 'south':
+            return isPositionAvailable(matrix, {
+                x: position.x - 1,
+                y: position.y
+            });
+        case 'west':
+            return isPositionAvailable(matrix, {
+                x: position.x,
+                y: position.y - 1
+            });
+        case 'north':
+            return isPositionAvailable(matrix, {
+                x: position.x + 1,
+                y: position.y
+            });
+    }
+}
+
+function getDirectionToLeft(direction) {
+    switch (direction) {
+        case 'east':
+            return 'north';
+        case 'south':
+            return 'east';
+        case 'west':
+            return 'south';
+        case 'north':
+            return 'west';
+    }
+}
+
+function getDirectionToRight(direction) {
+    switch (direction) {
+        case 'east':
+            return 'south';
+        case 'south':
+            return 'west';
+        case 'west':
+            return 'north';
+        case 'north':
+            return 'east';
+    }
+}
+
 export default function (robot) {
-    return isPositionInFrontAvailable(matrix, robot.position, robot.direction) ?
-        pathAhead(robot.position, robot.direction) :
-        obstacleAhead(robot.position, robot.direction);
+    // return isPositionInFrontAvailable(matrix, robot.position, robot.direction) ?
+    //     pathAhead(robot.position, robot.direction) :
+    //     obstacleAhead(robot.position, robot.direction);
+
+    if (isPositionToLeftOfTravelAvailable(matrix, robot.position, robot.direction)) {
+        return moveInDirection(robot.position, getDirectionToLeft(robot.direction));
+    } else if (isPositionInFrontAvailable(matrix, robot.position, robot.direction)) {
+        return moveInDirection(robot.position, robot.direction);
+    } else if (isPositionToRightOfTravelAvailable(matrix, robot.position, robot.direction)) {
+        return moveInDirection(robot.position, getDirectionToRight(robot.direction));
+    }
 }
